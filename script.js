03767fa21c522200424a234bcd1eefe0bd1d3490
janishi <giorgijanishi@gmail.com>
2024-02-04 15:44:58 +0400

    update2

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-function clicked() {
-  let h1 = document.querySelector("h1");
-  let ptag = document.querySelector("p");
-  h1.setAttribute("class", "active");
-  h1.style.backgroundColor = "yellow";
-  ptag.innerHTML = h1.getAttribute("class");
-}
+// function clicked() {
+//   let h1 = document.querySelector("h1");
+//   let ptag = document.querySelector("p");
+//   h1.setAttribute("class", "active");
+//   h1.style.backgroundColor = "yellow";
+//   ptag.innerHTML = h1.getAttribute("class");
+// }
 
-const foo = () => {
-  let input = document.getElementById("myInput").value;
-  let paragraph = document.getElementById("input_p");
-  paragraph.textContent = input;
-};
+// const foo = () => {
+//   let input = document.getElementById("myInput").value;
+//   let paragraph = document.getElementById("input_p");
+//   paragraph.textContent = input;
+// };
 
-let array = [1, 2, 3, 4, 5];
-let result = array.push(6);
-for (let item of array) {
-  console.log((item = item + 1) * 2);
+// let array = [1, 2, 3, 4, 5];
+// let result = array.push(6);
+// for (let item of array) {
+//   console.log((item = item + 1) * 2);
+// }
+
+let counter = 5;
+while (counter < 10) {
+  counter++;
+  console.log(counter);
 }
